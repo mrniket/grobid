3d993ccfb73c5eed9687e7198b2bcd6c66596f87
Niket Shah <masterniket@gmail.com>
2015-05-24 11:58:08 +0100

    add figure parser to the EngineParsers class

--- grobid-core/src/main/java/org/grobid/core/engines/EngineParsers.java
+++ grobid-core/src/main/java/org/grobid/core/engines/EngineParsers.java
@@ -25,6 +25,7 @@ public class EngineParsers implements Closeable {
     private ChemicalParser chemicalParser = null;
     private Segmentation segmentationParser = null;
     private ReferenceSegmenterParser referenceSegmenterParser = null;
+    private FigureParser figureParser = null;
 
 
     public AffiliationAddressParser getAffiliationAddressParser() {
@@ -139,6 +140,17 @@ public class EngineParsers implements Closeable {
         return chemicalParser;
     }
 
+    public FigureParser getFigureParser() {
+        if (figureParser == null) {
+            synchronized (this) {
+                if (figureParser == null) {
+                    figureParser = new FigureParser(this);
+                }
+            }
+        }
+        return figureParser;
+    }
+
     @Override
     public void close() throws IOException {
         LOGGER.debug("==> Closing all resources...");
@@ -193,6 +205,12 @@ public class EngineParsers implements Closeable {
             segmentationParser.close();
             segmentationParser = null;
             LOGGER.debug("CLOSING segmentationParser");
+        }
+
+        if (figureParser != null) {
+            figureParser.close();
+            figureParser = null;
+            LOGGER.debug("CLOSING figureParser");
         }
 
         LOGGER.debug("==>All resources closed");
